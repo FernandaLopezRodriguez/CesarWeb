a19019b2b28957c8e9672d088ceeec1d99431990
FernandaLopezRodriguez <35509634+FernandaLopezRodriguez@users.noreply.github.com>
2018-02-26 21:42:47 -0600

    P

--- Services/SeguridadService.cs
+++ Services/SeguridadService.cs
@@ -8,6 +8,26 @@ namespace CesarWeb.Services
         ///  Aquí deben hacer todo el código necesario para Desencriptar el mensaje
         public string DesEncriptar(string Mensaje, int clave)
         {
+            if (clave > 0 && clave < ABECEDARIO.Length)
+            {
+                for (int b = 0; b < Mensaje.Length; b++ )
+                {
+                    int car = string.IndexOf(ABECEDARIO, (Mensaje[b]));
+                    if (car != -1)
+                    {
+                        int posicion = car - clave;
+                        while (posicion < 0)
+                        {
+                            posicion = posicion + ABECEDARIO.Length;
+                        }
+                        Mensaje += ABECEDARIO[posicion];
+                    }
+                    else
+                    {
+                        Mensaje += Mensaje[b];
+                    }
+                }
+            }
             return Mensaje;
         }
 
@@ -16,6 +36,27 @@ namespace CesarWeb.Services
         ///  Aquí deben hacer todo el código necesario para Encriptar el mensaje
         public string Encriptar(string Mensaje, int clave)
         {
+
+            if (clave > 0 && clave < ABECEDARIO.Length )
+            {
+                for (int i = 0; i < Mensaje.Length; i++)
+                {
+                    int car = string.IndexOf(ABECEDARIO, (Mensaje[i]));
+                    if (car != -1)
+                    {
+                        int posicion = car + clave;
+                        while (posicion >=ABECEDARIO.Length )
+                        {
+                            posicion = posicion-ABECEDARIO.Length;
+                        }
+                        Mensaje += ABECEDARIO[posicion];
+                    }
+                    else
+                    {
+                        Mensaje += Mensaje[i];
+                    }
+                }
+            }
             return Mensaje;
         }
     }
